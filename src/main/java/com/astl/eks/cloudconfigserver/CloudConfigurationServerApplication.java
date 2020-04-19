4c89548b00c2735e06b05b5b9f9bf877224ca133
Andrei Stelea <stelea.andrei@gmail.com>
2020-04-19 14:47:50 +0300

    Initial commit

---
+++ src/main/java/com/astl/eks/cloudconfigserver/CloudConfigurationServerApplication.java
@@ -0,0 +1,17 @@
+package com.astl.eks.cloudconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class CloudConfigurationServerApplication {
+
+	public CloudConfigurationServerApplication() {}
+	
+	public static void main(String[] args) {
+		SpringApplication.run(CloudConfigurationServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/astl/eks/cloudconfigserver/CloudConfigurationServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.astl.eks.cloudconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudConfigurationServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
